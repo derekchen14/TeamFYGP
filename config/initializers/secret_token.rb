0425542af2e6cf7f517d30134bce5591c86a6e8a
Laura Ku <laura@coverhound.com>
2013-01-19 09:53:38 -0800

    Rails Base App Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fygp::Application.config.secret_token = '14fea5b6cc7fb889534efc937d92b2cb823a85ca7a0084081ef65e505be1b8082e1bbcf6fba81e53dfa6afa65b88c19726c606a0fdb42dba1207bf46f01a52e9'
